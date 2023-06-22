234fc5a15f5f20d0c009ff942d3acea59d715fc1
Vicente Balbontín Jara <vicente.balbontin@sansano.usm.cl>
2023-06-21 20:09:11 -0400

    mail-a-supervisor

--- src/app/componentes/finalizacion/finalizacion.component.ts
+++ src/app/componentes/finalizacion/finalizacion.component.ts
@@ -3,6 +3,8 @@ import { GestionarService } from '../../servicios/alumno/gestionar_practica.serv
 import { ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from "@angular/router"
+import { ObtenerDatosService } from 'src/app/servicios/alumno/obtener_datos.service';
+import { SupervisorService } from 'src/app/servicios/supervisor/supervisor.service';
 
 
 @Component({
@@ -14,8 +16,10 @@ export class FinalizacionComponent {
   id_estudiante: number = -1;
   id_practica: number = -1;
   private sub: any;
+  alumno: any = {};
+  practica: any = {};
 
-  constructor(private service: GestionarService, private route: ActivatedRoute, private _snackBar: MatSnackBar, private router: Router) {
+  constructor(private service: GestionarService, private serviceEstudiante: ObtenerDatosService,private serviceSupervisor: SupervisorService, private route: ActivatedRoute, private _snackBar: MatSnackBar, private router: Router) {
     this.sub = this.route.params.subscribe(params => {
       this.id_estudiante = +params['id']; // (+) converts string 'id' to a number
       this.id_practica = +params['n'];
@@ -34,7 +38,7 @@ export class FinalizacionComponent {
           resultado = { ...resultado, ...data };
         },
         error: error => {
-          this._snackBar.open("Se ha producido un error interno", "Cerrar", {
+          this._snackBar.open("Se ha producido un error interno (Finalizar practica)", "Cerrar", {
             panelClass: ['red-snackbar'],
             duration: 3000
           });
@@ -44,7 +48,65 @@ export class FinalizacionComponent {
             panelClass: ['green-snackbar'],
             duration: 3000
           })
-          this.router.navigate(['/'])
+          //obtener datos del estudiante
+          this.serviceEstudiante.obtener_alumno(this.id_estudiante).subscribe(
+            {
+              next: (data:any) => {
+                resultado = { ...resultado, ...data };
+              },
+              error: (error:any) => {
+                this._snackBar.open("Se ha producido un error interno (Obtener Alumno)", "Cerrar", {
+                  panelClass: ['red-snackbar'],
+                  duration: 3000
+                });
+              },
+              complete: () => {
+                this.alumno = resultado.body;
+                resultado = {};
+                //console.log("alumno",this.alumno);
+                this.serviceEstudiante.obtener_practica(this.id_estudiante).subscribe(
+                  {
+                    next: (data:any) => {
+                      resultado = { ...resultado, ...data };
+                    },
+                    error: (error:any) => {
+                      this._snackBar.open("Se ha producido un error interno (Obtener practica)", "Cerrar", {
+                        panelClass: ['red-snackbar'],
+                        duration: 3000
+                      });
+                    },
+                    complete: () => {
+                      this.practica = resultado.body;
+                      resultado = {};
+                      console.log("practica",this.practica.correo_supervisor);
+                      console.log("alumno",this.alumno.nombre);
+                      console.log("supervisor",this.practica.nombre_supervisor);
+                      this.serviceSupervisor.enviarLink(this.practica.correo_supervisor,this.practica.nombre_supervisor,this.alumno.nombre).subscribe(
+                        {
+                          next: (data:any) => {
+                            resultado = { ...resultado, ...data };
+                          },
+                          error: (error:any) => {
+                            console.log("enviar mail error",error);
+
+                            this._snackBar.open("Se ha producido un error interno", "Cerrar", {
+                              panelClass: ['red-snackbar'],
+                              duration: 3000
+                            });
+                          },
+                          complete: () => {
+                            console.log("Correo enviado");
+                          }
+                        }
+                      );
+                      
+                    }
+                  }
+                );
+              }
+            }
+          );
+          //this.router.navigate(['/'])
         }
       }
     );
