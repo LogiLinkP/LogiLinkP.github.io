67ab67589e3bb57bd8bb2dfcd17a4341784801d4
Harold Caballero <mati_67m@hotmail.com>
2023-06-26 21:56:40 -0400

    arreglado error que generaba mensaje de tabla vacia aunque no lo estuviera y problema error al ordenar tabla

--- src/app/vistas/resumen_practicas/resumen_practicas.component.ts
+++ src/app/vistas/resumen_practicas/resumen_practicas.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy, AfterViewInit } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
 import { GetDetallesAlumnoService } from '../../servicios/encargado/resumen_practicas.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'resumen_practicas',
@@ -7,26 +10,61 @@ import { GetDetallesAlumnoService } from '../../servicios/encargado/resumen_prac
   styleUrls: ['./resumen_practicas.component.css']
 })
 export class TablaComponent implements OnInit {
+  @ViewChild(DataTableDirective, { static: false })
+  dtElement: DataTableDirective;
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
 
   alumnos: any = [];
-  dtOptions: DataTables.Settings = {
-    language: {
-      url: 'assets/localisation/es-es.json'
-    }
-  };
 
-  constructor(private service: GetDetallesAlumnoService) {
+  constructor(private service: GetDetallesAlumnoService, private _snackBar: MatSnackBar) {
+    this.dtOptions = {
+      language: {
+        url: 'assets/localisation/es-es.json'
+      },
+      drawCallback: () => {
+        console.log(this.alumnos);
+      }
+    };
+
     let respuesta: any = {};
     this.service.full_estudiante_practicas().subscribe({
       next: (data: any) => {
         respuesta = { ...respuesta, ...data }
       },
-      error: (error: any) => console.log(error),
+      error: (error: any) => {
+        this.alumnos = [];
+        this._snackBar.open("Error al solicitar datos", "Cerrar", {
+          duration: 10000,
+          panelClass: ['red-snackbar']
+        });
+      },
       complete: () => {
-        this.alumnos = respuesta.body;
-        console.log(this.alumnos);
+        this.alumnos = respuesta.body.map((alumno: any) => {
+          alumno.consistencia_nota = alumno.consistencia_nota ? `${Math.round(100 * alumno.consistencia_nota)}%` : null;
+          alumno.consistencia_informe = alumno.consistencia_informe ? `${Math.round(100 * alumno.consistencia_informe)}%` : null;
+          return alumno;
+        });
+        this.rerender();
       }
     });
+  }
+  rerender(): void {
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      // Destroy the table first
+      dtInstance.destroy();
+      // Call the dtTrigger to rerender again
+      this.dtTrigger.next(0);
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.dtTrigger.next(0);
+  }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
   }
 
   ngOnInit() {
