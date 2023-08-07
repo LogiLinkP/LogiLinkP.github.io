3e7ebcc529f9c82f82d42d0a49e02ced2bfdd689
Joacodef <joacodef1@gmail.com>
2023-08-06 20:13:32 -0400

    cambio chico

--- src/app/vistas/resumen_practicas/resumen_practicas.component.ts
+++ src/app/vistas/resumen_practicas/resumen_practicas.component.ts
@@ -46,11 +46,11 @@ export class TablaComponent implements OnInit {
       },
       complete: () => {
         this.alumnos = respuesta.body.map((alumno: any) => {
-          alumno.consistencia_nota = alumno.consistencia_nota ? `${Math.round(100 * alumno.consistencia_nota)}%` : "--";
-          alumno.consistencia_informe = alumno.consistencia_informe ? `${Math.round(100 * alumno.consistencia_informe)}%` : "--";
-          alumno.nota_evaluacion = alumno.nota_evaluacion ? alumno.nota_evaluacion : "--";
-          alumno.interpretacion_nota = alumno.interpretacion_nota ? alumno.interpretacion_nota : "--";
-          alumno.interpretacion_informe = alumno.interpretacion_informe ? alumno.interpretacion_informe : "--";
+          alumno.consistencia_nota = alumno.consistencia_nota ? `${Math.round(100 * alumno.consistencia_nota)}%` : "—";
+          alumno.consistencia_informe = alumno.consistencia_informe ? `${Math.round(100 * alumno.consistencia_informe)}%` : "—";
+          alumno.nota_evaluacion = alumno.nota_evaluacion ? alumno.nota_evaluacion : "—";
+          alumno.interpretacion_nota = alumno.interpretacion_nota ? alumno.interpretacion_nota : "—";
+          alumno.interpretacion_informe = alumno.interpretacion_informe ? alumno.interpretacion_informe : "—";
           return alumno;
         });
         this.rerender();
