8ba893d5de4fc6860947991523ce426412a06b2a
rtallar <rtallar@gmail.com>
2023-08-07 18:34:21 -0400

    copiar scss

--- src/app/componentes/barra-lateral/barra-lateral.component.ts
+++ src/app/componentes/barra-lateral/barra-lateral.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-barra-lateral',
   templateUrl: './barra-lateral.component.html',
-  styleUrls: ['./barra-lateral.component.scss']
+  styleUrls: ['./barra-lateral.component.scss',
+              '../../../assets/scss/_sidebar.scss']
 })
 export class BarraLateralComponent {
 
