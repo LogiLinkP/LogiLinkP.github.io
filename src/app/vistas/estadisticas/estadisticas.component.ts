e4b33f1788feb24b62aec2c9cc4628927be52f2f
rtallar <rtallar@gmail.com>
2023-08-09 19:04:25 -0400

    scroll to top funcionando

--- src/app/vistas/estadisticas/estadisticas.component.ts
+++ src/app/vistas/estadisticas/estadisticas.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-estadisticas',
   templateUrl: './estadisticas.component.html',
-  styleUrls: ['./estadisticas.component.scss']
+  styleUrls: ['./estadisticas.component.scss',
+              '../../../assets/css/sb-admin-2.min.css',
+              '../../../assets/vendor/fontawesome-free/css/all.min.css']
 })
 export class EstadisticasComponent {
-
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  scrollToTop(): void {
+    this.document.body.scrollTop = 0;
+    this.document.documentElement.scrollTop = 0;
+  }
 }
