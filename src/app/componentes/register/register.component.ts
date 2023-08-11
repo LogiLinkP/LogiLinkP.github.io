490baf8ab7894bbc43edbda752b7615505a62ec0
Howlhoff <diego.beltran@sansano.usm.cl>
2023-08-10 20:46:50 -0400

    actualizacion registro
    actualizacion registro

--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -11,6 +11,10 @@ export class RegisterComponent implements OnInit {
   email: string;
   password: string;
   confirmPassword: string;
+  es_supervisor: boolean = false;
+  es_estudiante: boolean = false;
+  RUT: string;
+  extras = {};
 
   constructor(public usuario: UsuarioService) {}
 
@@ -18,7 +22,32 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    const user = {correo: this.email, password: this.password, cnfPwd: this.confirmPassword, nombre: this.nombre, es_encargado: false, es_supervisor: false, es_estudiante: true, es_admin: false};
-    this.usuario.register(this.email,this.password,this.confirmPassword,this.nombre,false,false,true,false).subscribe( {next: data => { user }, error: err => { console.log('Error de registro') } });
+    if(this.es_estudiante){
+      this.extras = {RUT: this.RUT};
+    }
+    if(this.es_supervisor){
+      this.extras = {};
+    }
+    const user = {correo: this.email, password: this.password, cnfPwd: this.confirmPassword, nombre: this.nombre, es_encargado: false, es_supervisor: this.es_supervisor, es_estudiante: this.es_estudiante, es_admin: false, extras: this.extras};
+    this.usuario.register(this.email,this.password,this.confirmPassword,this.nombre,false,this.es_supervisor,this.es_estudiante,false,this.extras).subscribe( {next: data => { user }, error: err => { console.log('Error de registro') } });
+  }
+
+  checkout(arg: any){
+    if(arg.target.value == "1"){
+      this.es_estudiante = true;
+      this.es_supervisor = false;
+    }
+    else if(arg.target.value = "2"){
+      this.es_supervisor = true;
+      this.es_estudiante = false;
+    }else{
+      this.es_estudiante = false;
+      this.es_supervisor = false;
+      this.reloadPage();
+    }
+  }
+
+  reloadPage(): void{
+    window.location.reload()
   }
 }
--- src/app/servicios/usuario/usuario.service.ts
+++ src/app/servicios/usuario/usuario.service.ts
@@ -14,17 +14,18 @@ export class UsuarioService {
   constructor(private http: HttpClient, private cookies: CookieService) { }
 
   login(email: string, password: string): Observable<any>{
-    const req = new HttpRequest('POST',`${this.url}/login`,{email,password});
+    const req = new HttpRequest('POST',`${this.url}/usuario/login`,{email,password});
     return this.http.request(req);
   }
 
-  register(email: string, password: string, cnfPwd: string ,nombre: string, es_encargado: boolean, es_supervisor: boolean, es_estudiante: boolean, es_admin: boolean): Observable<any>{
-    const req = new HttpRequest('POST',`${this.url}/register`,{email,password,cnfPwd,nombre,es_encargado,es_supervisor,es_estudiante,es_admin});
+  register(email: string, password: string, cnfPwd: string ,nombre: string, es_encargado: boolean, es_supervisor: boolean, es_estudiante: boolean, es_admin: boolean, extras: any): Observable<any>{
+    console.log('hola');
+    const req = new HttpRequest('POST',`${this.url}/usuario/register`,{email,password,cnfPwd,nombre,es_encargado,es_supervisor,es_estudiante,es_admin,extras});
     return this.http.request(req);
   }
 
   logout(): Observable<any>{
-    const req = new HttpRequest('POST',`${this.url}/logout`, { });
+    const req = new HttpRequest('POST',`${this.url}/usuario/logout`, { });
     return this.http.request(req);
   }
 
