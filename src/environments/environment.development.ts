880639e241f8091529544d6871491f2d7c093388
rtallar <rtallar@gmail.com>
2023-08-13 17:45:57 -0400

    update rutas en routing module

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,9 @@
 export const environment = {
     url_back: 'http://localhost:3000',
+    
     ruta_practicas: 'practicas',
-    ruta_alumno: 'alumno'
+    ruta_alumno: 'alumno',
+    ruta_login: 'login',
+    ruta_supervisor: 'supervisor',
+    ruta_registro: 'registro'
 };
