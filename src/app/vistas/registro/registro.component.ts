9b9fe3a6518f04fa5283a81ec32c2326af0cceba
rtallar <rtallar@gmail.com>
2023-08-14 16:32:26 -0400

    arreglo bug data binding en el registro

--- src/app/vistas/registro/registro.component.ts
+++ src/app/vistas/registro/registro.component.ts
@@ -13,8 +13,8 @@ export class RegistroComponent implements OnInit {
   email: string;
   password: string;
   confirmPassword: string;
-  es_supervisor: boolean = false;
-  es_estudiante: boolean = false;
+  es_supervisor: boolean;
+  es_estudiante: boolean;
   RUT: string;
   extras = {};
 
@@ -30,7 +30,7 @@ export class RegistroComponent implements OnInit {
       confirmPassword: ['', Validators.required],
       es_supervisor: [false],
       es_estudiante: [false],
-      RUT: ['']
+      RUT: []
       //crear validador custom para RUT real
     });
   }
@@ -39,14 +39,16 @@ export class RegistroComponent implements OnInit {
   }
 
   register() {
-    if(this.es_estudiante){
+    if (this.es_estudiante) {
       this.extras = {RUT: this.RUT};
     }
-    if(this.es_supervisor){
+    if (this.es_supervisor) {
       this.extras = {};
     }
-    const user = {correo: this.email, password: this.password, cnfPwd: this.confirmPassword, nombre: this.nombre, es_encargado: false, es_supervisor: this.es_supervisor, es_estudiante: this.es_estudiante, es_admin: false, extras: this.extras};
-    this.usuario.register(this.email,this.password,this.confirmPassword,this.nombre,false,this.es_supervisor,this.es_estudiante,false,this.extras).subscribe( {next: data => { user }, error: err => { console.log('Error de registro') } });
+    const data = this.registroForm.value;
+    const user = {correo: data.email, password: data.password, cnfPwd: data.confirmPassword, nombre: data.nombre, es_encargado: false, es_supervisor: data.es_supervisor, es_estudiante: data.es_estudiante, es_admin: false, extras: data.extras};
+    console.log(user);
+    this.usuario.register(data.email,data.password,data.confirmPassword,data.nombre,false,data.es_supervisor,data.es_estudiante,false,data.extras).subscribe( {next: data => { user }, error: err => { console.log('Error de registro') } });
   }
 
   checkout(arg: any){
