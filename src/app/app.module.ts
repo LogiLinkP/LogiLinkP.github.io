1eb22ff4ef7a8edabc012de0d0a6a4de7af1d81a
Joacodef <joacodef1@gmail.com>
2023-08-13 22:18:27 -0400

    front funcionando, pero componente de chat parece causar problemas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {FormsModule} from '@angular/forms';
 import { DatePipe } from '@angular/common';
-import { DatePipe } from '@angular/common';
 import {MatButtonModule } from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
@@ -16,16 +15,6 @@ import {MatSelectModule} from '@angular/material/select';
 import {TextFieldModule} from '@angular/cdk/text-field';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { ArchivosService } from './servicios/archivos/archivos.service';
-import { GetDetallesAlumnoService } from './servicios/encargado/resumen_practicas.service';
-import { SetDetallesAlumnoService } from './servicios/encargado/decision.service';
-import { SupervisorService } from './servicios/supervisor/supervisor.service';
-
-import { EvaluacionComponent } from './vistas/evaluacion_supervisor/evaluacion_supervisor.component';
-import { DetallePracticaComponent } from './vistas/detalle-practica/detalle-practica.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -50,19 +39,18 @@ import { FooterComponent } from './componentes/footer/footer.component';
 import { BarraSuperiorComponent } from './componentes/barra-superior/barra-superior.component';
 import { LogoutModalComponent } from './componentes/logout-modal/logout-modal.component';
 import { BarraLateralAlumnoComponent } from './componentes/barra-lateral-alumno/barra-lateral-alumno.component';
-import { InformacionesComponent } from './vistas/informaciones/informaciones.component';
-import { EstadisticasComponent } from './vistas/estadisticas/estadisticas.component';
-
-import { TestBarraComponent } from './componentes/test-barra/test-barra.component';
-import { LogoutComponent } from './componentes/logout/logout.component';
-import { FooterComponent } from './componentes/footer/footer.component';
-import { BarraSuperiorComponent } from './componentes/barra-superior/barra-superior.component';
-import { LogoutModalComponent } from './componentes/logout-modal/logout-modal.component';
-import { BarraLateralAlumnoComponent } from './componentes/barra-lateral-alumno/barra-lateral-alumno.component';
 import { BarraLateralEncargadoComponent } from './componentes/barra-lateral-encargado/barra-lateral-encargado.component';
-import { EstadisticasComponent } from './vistas/estadisticas/estadisticas.component';
-import { BotonSolicitarArchivoComponent } from './boton-solicitar-archivo/boton-solicitar-archivo.component';
-import { InformacionesComponent } from './vistas/informaciones/informaciones.component';
+import { DatosPracticaComponent } from './componentes/datos-practica/datos-practica.component';
+import { UpInformeComponent } from './componentes/up-informe/up-informe.component';
+import { TablaComponent } from './vistas/resumen_practicas/resumen_practicas.component';
+import { ChatComponent } from './componentes/chat/chat.component';
+
+
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataTablesModule } from "angular-datatables";
+import { CookieService } from 'ngx-cookie-service';
+import { BotonSolicitarArchivoComponent } from './componentes/boton-solicitar-archivo/boton-solicitar-archivo.component';
 
 
 
@@ -90,7 +78,7 @@ import { InformacionesComponent } from './vistas/informaciones/informaciones.com
     BotonSolicitarArchivoComponent,
     UpInformeComponent,
     TablaComponent,
-    ChatComponent
+    ChatComponent,
   ],
   imports: [
     BrowserModule,
