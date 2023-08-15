9dfdc935b9dabc9773794100214e458be8602b49
Vicente Balbontín Jara <vicente.balbontin@sansano.usm.cl>
2023-08-15 18:21:51 -0400

    error variables

--- src/app/componentes/barra-lateral-encargado/barra-lateral-encargado.component.ts
+++ src/app/componentes/barra-lateral-encargado/barra-lateral-encargado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-barra-lateral-encargado',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./barra-lateral-encargado.component.scss']
 })
 export class BarraLateralEncargadoComponent {
+  rutas = environment;
+  name: string;
+  practicas_creadas: any = [];
 
+  constructor() {}
+
+  set_practicas_creadas() {
+    this.practicas_creadas.push({name: this.name, id: this.practicas_creadas.length});
+  }
 }
