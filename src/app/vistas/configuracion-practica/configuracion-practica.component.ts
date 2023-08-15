f681d632fa172ee3ba9bb5db12505f5b0d484b31
rtallar <rtallar@gmail.com>
2023-08-15 18:36:21 -0400

    agregar data a formgroup

--- src/app/vistas/configuracion-practica/configuracion-practica.component.ts
+++ src/app/vistas/configuracion-practica/configuracion-practica.component.ts
@@ -28,27 +28,24 @@ export class ConfiguracionPracticaComponent implements OnInit {
     this.formdata = new FormGroup({
       nombrePractica: new FormControl("Practica 1"),
       horas: new FormControl(false),
-      meses: new FormControl(false)
+      meses: new FormControl(false),
+      cant_horas: new FormControl(),
+      cant_meses: new FormControl(),
+      frecuenciaInformes: new FormControl(),
+      informeFinal: new FormControl()
     });
   }
   onClickSubmit(data: any) {
     this.nombrePractica = data.nombrePractica;
     this.horas = data.horas;
     this.meses = data.meses;
+
     this.cant_horas = data.cant_horas;
     this.cant_meses = data.cant_meses;
-
-    
 
     this.frecuencia_informe = data.frecuenciaInformes;
     this.informe_final = data.informeFinal;
 
-    console.log(this.nombrePractica);
-    console.log(this.horas);
-    console.log(this.meses);
-    console.log(this.cant_horas);
-    console.log(this.cant_meses);
-    console.log(this.frecuencia_informe);
-    console.log(this.informe_final);
+    console.log(data);
   }
 }
