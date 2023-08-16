a3f09878a67d7ccb2f3c6292579f3ce67ab150c7
rtallar <rtallar@gmail.com>
2023-08-16 19:15:58 -0400

    mismo commit anterior, se me olvido agregar el archivo xd

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,10 @@
+export const commonEnv = {
+    ruta_practicas: 'practicas',
+    ruta_alumno: 'alumno',
+    ruta_login: 'login',
+    ruta_supervisor: 'supervisor',
+    ruta_registro: 'registro',
+    ruta_documento_extra: "documento_extra",
+    url_back: 'http://localhost:3000',
+    url_back_chat: 'http://localhost:5000'
+};
