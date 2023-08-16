66c131e5d53be1d2669c086ebb09d002bede167d
rtallar <rtallar@gmail.com>
2023-08-16 19:15:29 -0400

    arreglar environments
    mover rutas comunes a otro ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,9 @@
-export const environment = {
-    url_back: 'https://logilink.hopto.org',
-    url_back_chat: 'https://logilink.hopto.org:5000',
+import { commonEnv } from "./environment.common";
 
-    ruta_practicas: 'practicas',
-    ruta_alumno: 'alumno',
-    ruta_login: 'login',
-    ruta_supervisor: 'supervisor',
-    ruta_registro: 'registro',
-    ruta_documento_extra: "documento_extra"
-};
+const env: Partial<typeof commonEnv> = {
+    url_back: 'https://logilink.hopto.org',
+    url_back_chat: 'https://logilink.hopto.org:5000'
+  };
+
+// Export all settings of common replaced by dev options
+export const environment = Object.assign(commonEnv, env);
