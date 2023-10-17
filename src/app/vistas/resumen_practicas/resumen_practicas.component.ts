d787cfc235edf9b56f8d6da18485b67a5c908edc
Joacodef <joacodef1@gmail.com>
2023-10-17 11:52:10 -0300

    Fixeado ordenamiento de la tabla

--- src/app/vistas/resumen_practicas/resumen_practicas.component.ts
+++ src/app/vistas/resumen_practicas/resumen_practicas.component.ts
@@ -35,8 +35,8 @@ export class TablaComponent {
 
   booleanValue: boolean = true;
   
-  editables:any = [];
-  ev_values:any = [];
+  editables:any = {};
+  ev_values:any = {};
 
   ev_value:number = -1;
 
@@ -93,29 +93,31 @@ export class TablaComponent {
         });
       },
       complete: () => {
+        
+
         let temppracticas:any = [];
 
-        for (let alumno of respuesta.body){
+        for (let practicaux of respuesta.body){
           //console.log(alumno)
-          if (alumno?.modalidad?.config_practica?.id_carrera == this.carrera_encargado && alumno?.modalidad?.config_practica?.id_carrera != null){
-            temppracticas.push(alumno);
-            if(alumno.ev_encargado == null || alumno.ev_encargado == -1){
-              this.ev_values.push("-")
+          if (practicaux?.modalidad?.config_practica?.id_carrera == this.carrera_encargado && practicaux?.modalidad?.config_practica?.id_carrera != null){
+            temppracticas.push(practicaux);
+            if(practicaux.ev_encargado == null || practicaux.ev_encargado == -1){
+              this.ev_values[practicaux.id] = "-"
               }else{
-              this.ev_values.push(alumno.ev_encargado)
+              this.ev_values[practicaux.id]= practicaux.ev_encargado
               }
           }
         }
 
         
 
-        this.practicas = temppracticas.map((alumno: any) => {
+        this.practicas = temppracticas.map((practicaux: any) => {
 
-          alumno.consistencia_nota = alumno.consistencia_nota ? `${Math.round(100 * alumno.consistencia_nota)}%` : "—";
-          alumno.consistencia_informe = alumno.consistencia_informe ? `${Math.round(100 * alumno.consistencia_informe)}%` : "—";
-          alumno.nota_evaluacion = alumno.nota_evaluacion ? alumno.nota_evaluacion : "—";
-          alumno.interpretacion_nota = alumno.interpretacion_nota ? alumno.interpretacion_nota : "—";
-          alumno.interpretacion_informe = alumno.interpretacion_informe ? alumno.interpretacion_informe : "—";
+          practicaux.consistencia_nota = practicaux.consistencia_nota ? `${Math.round(100 * practicaux.consistencia_nota)}%` : "—";
+          practicaux.consistencia_informe = practicaux.consistencia_informe ? `${Math.round(100 * practicaux.consistencia_informe)}%` : "—";
+          practicaux.nota_eval = practicaux.nota_eval ? practicaux.nota_eval : "—";
+          practicaux.interpretacion_nota = practicaux.interpretacion_nota ? practicaux.interpretacion_nota : "—";
+          practicaux.interpretacion_informe = practicaux.interpretacion_informe ? practicaux.interpretacion_informe : "—";
           
           /*alumno ={... alumno, carrera: this.carreras[Math.floor(Math.random() * this.carreras.length)]}
           console.log(alumno);
@@ -124,12 +126,13 @@ export class TablaComponent {
             this.practicas.splice(index, 1);
           }
           */
-          return alumno;
+          return practicaux;
         });
-        for (var item of this.practicas){
-          this.editables.push("0");
-          let iditem = item.id
-          this.practi_service.obtener_practica(item.id).subscribe({
+        console.log(this.practicas)
+        for (var practicaux of this.practicas){
+          this.editables[practicaux.id] = "0"
+          let id_practicaux = practicaux.id
+          this.practi_service.obtener_practica(id_practicaux).subscribe({
             next: (data: any) => {
               respuesta = { ...respuesta, ...data }
             },
@@ -144,7 +147,7 @@ export class TablaComponent {
               //console.log(evaluaciones)
               
               if(evaluaciones.length == 0){
-                this.temp_notas.push([iditem, 0])
+                this.temp_notas.push([id_practicaux, 0])
               }
               else{
                 let find = -1
@@ -162,24 +165,24 @@ export class TablaComponent {
                         prom += 1;
                       } 
                       nota_promedio = nota_promedio/prom
-                      this.temp_notas.push([iditem, nota_promedio])  
+                      this.temp_notas.push([id_practicaux, nota_promedio])  
                       break
                     }
                   }
                 }
                 if (find == -1){
-                  this.temp_notas.push([iditem, 0])
+                  this.temp_notas.push([id_practicaux, 0])
                 } 
               }
               this.temp_notas.sort(function(a:any, b:any){
                 return a[0] - b[0];
               })
               this.notas_promedio = [];
-              for (let item2 of this.temp_notas){
-                if (item2[1] == 0){
+              for (let nota_temp of this.temp_notas){
+                if (nota_temp[1] == 0){
                   this.notas_promedio.push("-")
                 } else{
-                  this.notas_promedio.push(item2[1])
+                  this.notas_promedio.push(nota_temp[1])
                 }
               }
               //console.log(this.notas_promedio) 
@@ -216,7 +219,7 @@ export class TablaComponent {
   sort(n:number) {
     this.sortedColumn = n;
     this.sortOrder = this.booleanValue ? 1 : -1;
-    console.log(this.booleanValue, this.sortOrder)
+    //console.log(this.booleanValue, this.sortOrder)
     if (this.booleanValue == false){
       switch(n){
         case 1:
@@ -270,39 +273,6 @@ export class TablaComponent {
                                                 a.estado > b.estado ? -1 :
                                                 0 
           )
-          break;
-        case 5:
-          this.practicas.sort((a:any, b:any) => a.indice_repeticion < b.indice_repeticion ? 1 :
-                                                a.indice_repeticion > b.indice_repeticion ? -1 :
-                                                0 
-          )
-          break;
-        case 6:
-          this.practicas.sort((a:any, b:any) => a.consistencia_informe < b.consistencia_informe ? 1 :
-                                                a.consistencia_informe > b.consistencia_informe ? -1 :
-                                                0 
-          )
-          break;
-        case 7:
-          this.practicas.sort((a:any, b:any) => a.consistencia_nota < b.consistencia_nota ? 1 :
-                                                a.consistencia_nota > b.consistencia_nota ? -1 :
-                                                0 
-          )
-          break;
-        case 8:
-          this.practicas.sort((a:any, b:any) => a.interpretacion_nota < b.interpretacion_nota ? 1 :
-                                                a.interpretacion_nota > b.interpretacion_nota ? -1 :
-                                                0 
-          )
-          break;
-        case 9: 
-          this.practicas.sort((a:any, b:any) => a.interpretacion_informe < b.interpretacion_informe ? 1 :
-                                                a.interpretacion_informe > b.interpretacion_informe ? -1 :
-                                                0 
-          )
-          break;
-        case 10:
-          this.notas_promedio.sort((a:any, b:any) => a < b ? 1 : a > b ? -1 : 0)
       }
       this.booleanValue = !this.booleanValue
     }
@@ -322,17 +292,17 @@ export class TablaComponent {
     }  
   }
 
-  editar(index:number){
-    this.editables[index] = "1"
+  editar(id_practica:number){
+    this.editables[id_practica] = "1"
   }
 
   checkout(arg: any) {
     this.ev_value = Number(arg.target.value)
   }
 
-  evaluacion_encargado(id_practica:number, index:number){
+  evaluacion_encargado(id_practica:number){
     if(this.ev_value == -1){
-      this.editables[index] = "0"
+      this.editables[id_practica] = "0"
       return;
     }
     let respuesta:any = [];
@@ -347,8 +317,8 @@ export class TablaComponent {
       complete:() => {
       }
     })
-    this.editables[index] = "0"
-    this.ev_values[index] = this.ev_value;
+    this.editables[id_practica] = "0"
+    this.ev_values[id_practica] = this.ev_value;
     this.ev_value = -1
   }
 
