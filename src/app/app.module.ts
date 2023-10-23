35ef4ddd481bf2f81ee62c786613cda958b33d3a
Joacodef <joacodef1@gmail.com>
2023-10-23 10:55:18 -0300

    Nueva vista confirmacion de correo, al registrarse y logearse se buscan usuarios con activado=true

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -91,6 +91,7 @@ import { ComentariosModalComponent } from './componentes/comentarios-modal/comen
 import { DocumentacionComponent } from './vistas/documentacion/documentacion.component';
 import { SubirDocumentoEncargadoComponent } from './componentes/subir-documento-encargado/subir-documento-encargado.component';
 import { SubirPlantillaInformeFinalComponent } from './componentes/subir-plantilla-informe-final/subir-plantilla-informe-final.component';
+import { ConfirmacionUsuarioComponent } from './vistas/confirmacion-usuario/confirmacion-usuario.component';
 
 @NgModule({
   declarations: [
@@ -148,6 +149,7 @@ import { SubirPlantillaInformeFinalComponent } from './componentes/subir-plantil
     DocumentacionComponent,
     SubirDocumentoEncargadoComponent,
     SubirPlantillaInformeFinalComponent,
+    ConfirmacionUsuarioComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/vistas/confirmacion-usuario/confirmacion-usuario.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { UsuarioService } from '../../servicios/usuario/usuario.service';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-confirmacion-usuario',
+  templateUrl: './confirmacion-usuario.component.html',
+  styleUrls: ['./confirmacion-usuario.component.scss']
+})
+export class ConfirmacionUsuarioComponent {
+
+  correo_confirmado: boolean = false;
+  usuario: any = {};
+  
+  constructor(activated_route: ActivatedRoute, private usuario_service: UsuarioService) {
+    let token = "";
+    let iv = "";
+    let response: any = {};
+
+    //obtener token y iv de la url
+    activated_route.queryParams.subscribe(params => {
+      token = params['token'];
+      iv = params['iv'];
+    });
+
+    //obtener usuario
+    this.usuario_service.get_usuario_encriptado(token, iv).subscribe({
+      next: (data: any) => {
+        response = { ...response, ...data };
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+      complete: () => {
+        if (response) {
+          this.correo_confirmado = true;
+          this.usuario = response.body.userdata;
+          console.log(this.usuario);
+        }
+      }
+    });
+
+   }
+
+
+
+}
