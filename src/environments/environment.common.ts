61a209967002be2f6827f7b9a306c32e0d35b6bb
Lirlisa <42386589+Lirlisa@users.noreply.github.com>
2024-06-04 21:06:03 -0400

    Vm test (#418)
    Cambios que facilitan el despliegue en servidor de producción
    
    **Aún no aprobar**

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -8,9 +8,9 @@ export const commonEnv = {
     ruta_documento: "documento",
     ruta_informe: "informe",
     ruta_reset_pass: "resetPass",
-    url_back: 'https://logilink.hopto.org:3000',
-    url_back_chat: 'https://logilink.hopto.org:5000',
-    url_front: 'https://logilink.hopto.org',
+    url_back: 'https://praxusback.hopto.org:3000',
+    url_back_chat: 'https://praxusback.hopto.org:5000',
+    url_front: 'https://praxus.hopto.org',
     estado_practica: {
         no_iniciada: 'No iniciada',
         en_curso: 'En curso',
